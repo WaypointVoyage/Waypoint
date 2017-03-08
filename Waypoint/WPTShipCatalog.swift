1cec7b6a42ec09f1e2ac819f27963008baa52016
cameron <axis7818@gmail.com>
2017-03-07 18:30:17 -0800

    load ships into a catalog

--- Waypoint/WPTShipCatalog.swift
+++ Waypoint/WPTShipCatalog.swift
@@ -9,12 +9,25 @@
 import SpriteKit
 
 class WPTShipCatalog {
+    static let allShips: [WPTShip] = {
+        var ships = [WPTShip]();
+        
+        for shipSet in [("the_ships", true), ("the_enemy_ships", false)] {
+            let path = Bundle.main.path(forResource: shipSet.0, ofType: "plist")!
+            for ship in NSArray(contentsOfFile: path) as! [[String:AnyObject]] {
+                ships.append(WPTShip(dict: ship, playable: shipSet.1))
+            }
+        }
+        
+        return ships;
+    }()
     
+    static let playableShips: [WPTShip] = WPTShipCatalog.allShips.filter() { $0.playable }
     static let shipsByName: [String:WPTShip] = {
-        var ships = [String:WPTShip]()
-        
-        
-        
-        return ships
+        var map = [String:WPTShip]();
+        for ship in WPTShipCatalog.allShips {
+            map[ship.name] = ship;
+        }
+        return map;
     }()
 }
