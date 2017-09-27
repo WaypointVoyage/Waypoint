164778895ef64fb119c048d54885dd7f85d24cc0
Cameron Taylor <axis7818@gmail.com>
2017-09-26 17:14:02 -0700

    initial tests, testing catalogs

---
+++ WaypointContentTests/WPTCatalogTests.swift
@@ -0,0 +1,45 @@
+//
+//  WPTCatalogTests.swift
+//  WaypointContentTests
+//
+//  Created by Cameron Taylor on 9/26/17.
+//  Copyright © 2017 cpe436group. All rights reserved.
+//
+
+import XCTest
+@testable import Waypoint
+
+class WPTCatalogTests: XCTestCase {
+    
+    func testWPTEnemyCatalog() {
+        
+        let _ = WPTEnemyCatalog.allBrainTemplates
+        let _ = WPTEnemyCatalog.allEnemies
+        let _ = WPTEnemyCatalog.brainTemplatesByName
+        let _ = WPTEnemyCatalog.enemiesByName
+        
+    }
+    
+    func testWPTItemCatalog() {
+        
+        let _ = WPTItemCatalog.allItems
+        let _ = WPTItemCatalog.currencyItems
+        let _ = WPTItemCatalog.itemsByName
+        let _ = WPTItemCatalog.repairItems
+        
+        let randRepair = WPTItemCatalog.randomRepair
+        let _ = randRepair()
+        let randCurrency = WPTItemCatalog.randomCurrency
+        let _ = randCurrency()
+        let randStatModifier = WPTItemCatalog.randomStatModifier
+        let _ = randStatModifier()
+        
+    }
+    
+    func testWPTShipCatalog() {
+        let _ = WPTShipCatalog.allShips
+        let _ = WPTShipCatalog.playableShips
+        let _ = WPTShipCatalog.shipsByName
+    }
+    
+}
