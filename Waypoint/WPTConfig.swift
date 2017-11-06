31a55365748c04a883d399bbbeae315be8dfb329
Hilary Schulz <hils124@yahoo.com>
2017-11-06 11:43:25 -0800

    Added more sound effects

--- Waypoint/WPTConfig.swift
+++ Waypoint/WPTConfig.swift
@@ -22,8 +22,8 @@ class WPTConfig {
     let playSoundEffects = true     // toggle sound effects
     
     // testing
-    var testing = false                  // toggles general testing features
-    var allUnlocked = false              // determines if all of the levels on the world map are unlocked and beaten
+    var testing = true                  // toggles general testing features
+    var allUnlocked = true              // determines if all of the levels on the world map are unlocked and beaten
     var freeItems = false                // if true, all items are free
     var invincible = false               // if true, the player is invincible
     var clearHighScoresOnLoad = false    // if true, all of the high scores are cleared when the app is loaded
